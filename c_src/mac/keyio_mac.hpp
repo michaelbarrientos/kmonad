b2432702d743ddb806b8674e1a736795eeef15bc
Michael Barrientos <minted_modeler0x@icloud.com>
2024-01-07 20:13:33 -0600

    macOS ignore "Karabiner*" keyboards; necessary if iokit-name is specified w/o arguments.

--- c_src/mac/keyio_mac.hpp
+++ c_src/mac/keyio_mac.hpp
@@ -75,7 +75,8 @@ void open_matching_devices(char *product, io_iterator_t iter) {
             return;
         }
     }
-    CFStringRef cfkarabiner = CFStringCreateWithCString(kCFAllocatorDefault, "Karabiner VirtualHIDKeyboard", CFStringGetSystemEncoding());
+    CFStringRef cfkarabiner = CFStringCreateWithCString(kCFAllocatorDefault,
+	"Karabiner", CFStringGetSystemEncoding());
     if(cfkarabiner == NULL) {
         print_iokit_error("CFStringCreateWithCString");
         if(product) {
@@ -89,7 +90,7 @@ void open_matching_devices(char *product, io_iterator_t iter) {
             print_iokit_error("IORegistryEntryCreateCFProperty");
             continue;
         }
-        bool match = (CFStringCompare(cfcurr, cfkarabiner, 0) != kCFCompareEqualTo);
+	bool match = !CFStringHasPrefix(cfcurr, cfkarabiner);
         if(product) {
             match = match && (CFStringCompare(cfcurr, cfproduct, 0) == kCFCompareEqualTo);
         }
